b8128acf97b71acf0df702dc4b747fcbd6cc2760
EshginGuluzade <67023632+EshginGuluzade@users.noreply.github.com>
2020-06-17 00:09:21 +0400

    Update Problem 3 Valid words____is_valid_word(word, hand, word_list).py

--- Problem 3 Valid words____is_valid_word(word, hand, word_list).py
+++ Problem 3 Valid words____is_valid_word(word, hand, word_list).py
@@ -1,3 +1,32 @@
+'''
+# mit_course_number_6.0001_problem_set3_solutions
+MIT Course: Introduction to Computer Science and Programming in Python
+
+Author: Eshgin Guluzade
+
+Specialty: Mechanical Engineer
+
+Country: Azerbaijan
+'''
+
+def get_word_score(word, n):
+     points = 0
+     second_component = 0
+     word = word.lower()
+     
+     for i in word:
+             if i in SCRABBLE_LETTER_VALUES:
+                 points = points + SCRABBLE_LETTER_VALUES[i]
+             else:
+                 continue
+     
+     second_component = (7*len(word)) - (3 * (n - len(word)))
+     
+     if second_component < 1:
+         second_component = 1
+     
+     return points * second_component
+
 def is_valid_word(word, hand, word_list):
     
      word = word.lower()
