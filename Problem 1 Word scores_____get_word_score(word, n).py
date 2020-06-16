2c2f1e8b57f044528730bad048ef795f379de2ac
EshginGuluzade <67023632+EshginGuluzade@users.noreply.github.com>
2020-06-17 00:06:45 +0400

    Update Problem 1 Word scores_____get_word_score(word, n).py

--- Problem 1 Word scores_____get_word_score(word, n).py
+++ Problem 1 Word scores_____get_word_score(word, n).py
@@ -1,3 +1,14 @@
+'''
+# mit_course_number_6.0001_problem_set3_solutions
+MIT Course: Introduction to Computer Science and Programming in Python
+
+Author: Eshgin Guluzade
+
+Specialty: Mechanical Engineer
+
+Country: Azerbaijan
+'''
+
 def get_word_score(word, n):
      points = 0
      second_component = 0
