62aaecb3b94b2c3faeeb28999960ecf0a1262e2a
EshginGuluzade <67023632+EshginGuluzade@users.noreply.github.com>
2020-06-17 00:33:25 +0400

    Update Problem 2 Dealing with hands____display_hand(hand).py

--- Problem 2 Dealing with hands____display_hand(hand).py
+++ Problem 2 Dealing with hands____display_hand(hand).py
@@ -1,12 +1,19 @@
 '''
 # mit_course_number_6.0001_problem_set3_solutions
-MIT Course: Introduction to Computer Science and Programming in Python
+
+MIT Course 6.0001: Introduction to Computer Science and Programming in Python
 
 Author: Eshgin Guluzade
 
 Specialty: Mechanical Engineer
 
 Country: Azerbaijan
+
+My LinkedIn: https://www.linkedin.com/in/eshginguluzade/
+
+My Quora: https://www.quora.com/profile/Eshgin-Guluzade
+
+My Youtube Channel: https://www.youtube.com/channel/UCr6yufueEVaXBHgM8x1YlTw
 '''
 
 def get_word_score(word, n):
